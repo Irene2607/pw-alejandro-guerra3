7f724f400f81d0cdfb0e8b051481bacfa6ca51bb
Irene2607 <lalejandro@est.ups.edu.ec>
2022-03-05 18:04:46 -0500

    token

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,18 +1,25 @@
 import User from '../models/User'
+import jwt from 'jsonwebtoken'
+import config from '../config';
 
 export const signUp = async (req, res) => {
     const {username, email, password, roles} = req.body;
 
+
     const newUser = new User({
         username,
         email,
-        password: User.encryptPassword(password)
+        password: await User.encryptPassword(password)
     })
-    console.log(newUser)
-    // await newUser.save();
-    res.json('signup')
+ 
+    const savedUser = await newUser.save();
 
+    const token = jwt.sign({id: savedUser._id}, config.SECRET, {
+        expiresIn: 86400 //24horas
+    })
+    res.status(200).json({token})
 }
+
 export const signIn = async (req, res) => {
     res.json('signin')
 
